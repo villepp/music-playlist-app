d099f392c51e5b9b1219898070d6dbcd8411f507
Ville <ville777@protonmail.com>
2023-10-31 23:45:03 +0200

    eyeye
    ayayay

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, request, abort
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+videos = [
+    {"id": 1, "url": "https://www.youtube.com/watch?v=yNVg2qaAtnI", "title": "Video 1"},
+    {"id": 2, "url": "https://www.youtube.com/watch?v=_6L6E7TLy10", "title": "Video 2"},
+]
+
+@app.route('/api/videos', methods=['GET'])
+def get_videos():
+    return jsonify(videos)
+
+@app.route('/api/videos', methods=['POST'])
+def add_video():
+    if not request.json or 'url' not in request.json:
+        abort(400)
+    video = {
+        'id': videos[-1]['id'] + 1,
+        'url': request.json['url'],
+        'title': request.json.get('title', "")
+    }
+    videos.append(video)
+    return jsonify(video), 201
+
+@app.route('/api/videos/<int:video_id>', methods=['DELETE'])
+def delete_video(video_id):
+    video = next((video for video in videos if video['id'] == video_id), None)
+    if video is None:
+        abort(404)
+    videos.remove(video)
+    return jsonify({'result': True})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
